c825caeed61646e6ad3905736f47da0a3e3d8969
Maxilinares12 <maxilinares11@gmail.com>
2022-07-17 19:36:42 -0300

    cambios

---
+++ menu.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-md mb-4">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link px-4 py-1" href="index.html">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-4 py-1 active" href="sobre-mi.html">Sobre mí</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-4 py-1" href="proyectos.html">Proyectos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-4 py-1" href="contacto.html">Contacto</a>
+                        </li>
+                    </ul>
+                    <div class="d-inline btn-rojo">
+                        <a href="" class="btn-descarga">Descargar mi CV</a>
+                        <i class="fa-solid fa-download"></i>
+                    </div>
+                </div>
+            </div>
+        </nav>
