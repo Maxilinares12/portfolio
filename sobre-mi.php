ff3f105fca8204f1e55b8a15ed63805b61a80379
Maxilinares12 <maxilinares11@gmail.com>
2022-07-17 20:00:53 -0300

    cambios

--- sobre-mi.php
+++ sobre-mi.php
@@ -1,3 +1,8 @@
+<?php
+$pg = "sobre-mi";
+?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -15,34 +20,7 @@
 
 <body id="sobre-mi">
     <header class="container">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="index.html">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1 active" href="sobre-mi.html">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="proyectos.html">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="contacto.html">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-inline btn-rojo">
-                        <a href="" class="btn-descarga">Descargar mi CV</a>
-                        <i class="fa-solid fa-download"></i>
-                    </div>
-                </div>
-            </div>
-        </nav>
+    <?php include_once "menu.php";?>
     </header>
     <main>
         <section class="container">
